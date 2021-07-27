4d40b15275f040e3018ddf2dc933645e3a305e0f
kcj3054 <kcj3054@naver.com>
2021-07-28 01:02:06 +0900

    vue-todo

--- vue-todo/src/components/TodoInput.vue
+++ vue-todo/src/components/TodoInput.vue
@@ -3,9 +3,9 @@
       <!-- v-model input에 입력된것을 vue 데이터에 맵핑한다  -->
       <input type="text" v-model="newTodoItem" v-on:keyup.enter="addTodo">
       <!-- <button v-on:click="addTodo">add</button> -->
-      <span class="addContainer" v-on:click="addTodo">
-          <i class="fas fa-plus"></i>
-      </span>
+    <span class="addContainer" v-on:click="addTodo">
+      <i class="addBtn fas fa-plus" aria-hidden="true"></i>
+    </span>
   </div>
 </template>
 
@@ -32,32 +32,28 @@ export default {
 </script>
 
 <style scoped>
-
 input:focus {
-    outline: none;
+  outline: none;
 }
-
 .inputBox {
-    background: white;
-    height: 50px;
-    line-height: 50px;
-    border-radius: 5px;
+  background: white;
+  height: 50px;
+  line-height: 50px;
+  border-radius: 5px;
 }
-
 .inputBox input {
-    border-style: none;
-    font-size: 0.9rem;
+  border-style: none;
+  font-size: 0.9rem;
 }
-
 .addContainer {
-    float: right;
-    background: linear-gradient(to right, #6478FB, #8963FB);
-    display: block;
-    width: 3rem;
-    border-radius: 0 5px 5px 0;
+  float: right;
+  background: linear-gradient(to right, #6478FB, #8763FB);
+  display: block;
+  width: 3rem;
+  border-radius: 0 5px 5px 0;
 }
 .addBtn {
-    color: white;
-    vertical-align: middle;
+  color: white;
+  vertical-align: middle;
 }
 </style>
---
+++ vue-todo/src/store/store.js
@@ -0,0 +1,13 @@
+import Vue  from 'vue'
+import Vuex from 'vuex'
+
+
+Vue.use(Vuex);
+
+//Todo.vue
+
+
+//store를 다른쪽에서도 사용 가능 하다 
+export const store = new Vuex.Store({
+
+});
